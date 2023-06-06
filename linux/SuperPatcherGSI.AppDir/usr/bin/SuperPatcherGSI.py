0259e22aa81aef78c0749150e7c86ac1db923fc7
ChromiumOS-Guy <57168079+ChromiumOS-Guy@users.noreply.github.com>
2023-06-06 14:09:53 +0300

    Add files via upload

--- linux/SuperPatcherGSI.AppDir/usr/bin/SuperPatcherGSI.py
+++ linux/SuperPatcherGSI.AppDir/usr/bin/SuperPatcherGSI.py
@@ -10,7 +10,6 @@ HERE = os.path.realpath(os.path.dirname(__file__))
 parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument('-i' , '--input' , type=argparse.FileType('r') , help='''input the super.img that is going to be modifed, if super.img is sparse its going to temporarily be unsparsed''')
 parser.add_argument('-o' , '--output', help="Directs the output to a name of your choice")
-parser.add_argument('-g' , '--gsi' , type=argparse.FileType('r') , help="GSI (Generic System Image) that will be replacing the existing system.img (Stock Rom)")
 parser.add_argument('-s' , '--SLOT' , type=int , help="number of slots on the device can only be 1 (A) or 2 (A/B)")
 args = parser.parse_args()
 # 
@@ -42,11 +41,6 @@ def check():
     else:
         print("Invalid Format at INPUT please use .img file")
         err += " &InvalidFormatOUTPUT"
-    if args.gsi.name.endswith(".img"):
-        pass
-    else:
-        print("Invalid Format at GSI please use .img file")
-        err += " &InvalidFormatGSI"
     if err == "":
         err = "OK"
     return err
@@ -67,18 +61,40 @@ def IMGchoose(): # choose an img file to be replaced
         try:
             imgnum = input("Please Choose: ")
             if int(imgnum) <= i - 1:
-                return TempImgList[int(imgnum)]
+                replacmentpath = ""
+                while (True):
+                    try:
+                        replacmentpath = input("Please Input Path To Replacment Partition:\n")
+                        if replacmentpath.endswith(".img"):
+                            break
+                        else:
+                            print("Please Input a Valid Path to a IMG File!")
+                    except ValueError:
+                        print("Please Input a Valid Path to a IMG File!")
+                shutil.copy(replacmentpath , TempDIR + "/" + TempImgList[int(imgnum)])
+                redo = input("img replaced!\nreplace another Y/n: ")
+                if redo == "Y" or redo == "y" or redo == "yes" or redo == "Yes": # just making sure
+                    IMGchoose()
+                break
             else:
                 print("Please Put a Valid Number!")
         except ValueError:
             print("Please Put a Number In!")
 
-def IMGreplace(TempImgName): # replace img with GSI
-    shutil.copy(args.gsi.name , TempDIR + "/" + TempImgName)
+def IMGsizeCALC(): # calculate size
+    totalsize = 512000000 # extra overhead of 0.5G
+    TempImgList = os.listdir(TempDIR)
+    i = 0
+    for img in TempImgList:
+        if img.endswith(".img"):
+            totalsize += os.path.getsize(TempDIR + "/" + img)
+        i += 1
+    reminder = totalsize % 512 # making devision by 512
+    totalsize += reminder
+    return totalsize
 
 # lpmake
 def lpmake(devicesize , metadatasize):
-    
     lpmake_args = " --device-size={devicesize}".format(devicesize=devicesize) + " --metadata-slots={slot}".format(slot=args.SLOT) + " --output {output}".format(output=args.output) + " --metadata-size {metadatasize}".format(metadatasize=metadatasize)
     sparse = input("make sparse (flashable with fastboot) ? (Y/n): ")
     if sparse == "Y" or sparse == "y" or sparse == "yes" or sparse == "Yes" or sparse == "": # just making sure
@@ -91,7 +107,7 @@ def lpmake(devicesize , metadatasize):
     print(lpmake_args)
     print("============================")
     
-    os.system("{dir}/bin/lpmake {lpargs}".format(lpargs=lpmake_args , dir=HERE))
+    return os.system("{dir}/bin/lpmake {lpargs}".format(lpargs=lpmake_args , dir=HERE))
 
 def lpmake_add_args(lpmake_args):
     TempImgList = os.listdir(TempDIR)
@@ -126,34 +142,33 @@ def main():
     print("============================")
     
     
-    IMGreplace(IMGchoose()) # replaces selcted partition with GSI
-    
+    IMGchoose() # replaces selcted partition with GSI
     #let user choose size
     metadatasize = 512000
-    devicesize = 8192000000
+    devicesize = IMGsizeCALC()
     print("============================")
     try:
-        dvsize = input("device size (super.img size) in bytes must be evenly divisible by 512 defualt=~8G: ")
+        dvsize = input("device size (super.img size) in bytes must be evenly divisible by 512, defualt ({devicesize}) bytes: ".format(devicesize=devicesize))
         if dvsize != "" and testdvi512(int(dvsize)):
             devicesize = int(dvsize)
     except ValueError:
         print("Invalid Number skipping ..!")
     
     try:
-        mdsize = input("metadata size in bytes must be evenly divisible by 512 defualt=~0.5G: ")
+        mdsize = input("metadata size in bytes must be evenly divisible by 512 defualt=~0.5KiB: ")
         if mdsize != "" and testdvi512(int(dvsize)):
             metadatasize = int(mdsize)
     except ValueError:
         print("Invalid Number skipping ..!")
     
     #repack
-    lpmake(devicesize , metadatasize)
+    lperr = lpmake(devicesize , metadatasize)
+    err = lperr if lperr != 0 else err # give lpmake error as external code if there was an error
     print("============================")
     print("        cleaning...")
     print("============================")
     shutil.rmtree(TempDIR) # clean tmp dir
     return err # return err code to external
-    
 
 err = main()
 exit(err)
