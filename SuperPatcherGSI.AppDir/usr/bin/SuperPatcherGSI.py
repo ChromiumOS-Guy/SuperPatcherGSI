2d76d73f266eb37341a72962257629d7e5c3553d
ChromiumOS-Guy <57168079+ChromiumOS-Guy@users.noreply.github.com>
2025-09-27 07:38:09 +0300

    fixed indent in L172

--- SuperPatcherGSI.AppDir/usr/bin/SuperPatcherGSI.py
+++ SuperPatcherGSI.AppDir/usr/bin/SuperPatcherGSI.py
@@ -169,9 +169,9 @@ def IMGmanipulation(): # choose an img file to be replaced
 # lpmake
 def lpmake(devicesize , metadatasize): # fucntion for assembling lpmake flags 
     lpmake_args = " --device-size={devicesize}".format(devicesize=devicesize) # add size of super.img
-        + " --metadata-slots={slot}".format(slot=args.SLOT) # define type of device the super.img is for s=1 (A) s=2 (A/B)
-        + " --output {output}".format(output=args.output) # define output path
-        + " --metadata-size {metadatasize}".format(metadatasize=metadatasize) # define metadata size
+    lpmake_args += " --metadata-slots={slot}".format(slot=args.SLOT) # define type of device the super.img is for s=1 (A) s=2 (A/B)
+    lpmake_args += " --output {output}".format(output=args.output) # define output path
+    lpmake_args += " --metadata-size {metadatasize}".format(metadatasize=metadatasize) # define metadata size metadata size
     sparse = input("make sparse (flashable with fastboot) ? (Y/n): ") # ask user if they want to sparse super.img
     if sparse == "Y" or sparse == "y" or sparse == "yes" or sparse == "Yes" or sparse == "": # just making sure
         lpmake_args += " --sparse"
